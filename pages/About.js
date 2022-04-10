0f0e1460a67f5da67da2a9e0bfddb80c37033309
ashok.chakravarthi <ashok.chakravarthi@zohocorp.com>
2022-04-10 13:14:25 +0530

    About page update

--- pages/About.js
+++ pages/About.js
@@ -14,9 +14,9 @@ export default function About() {
 		<Layout>
 			<div>
 			<div className="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
+  <h1>About Us</h1>
+  <p>We are a small office and home like office, located in New York City.</p>
+  <p>But we are growing fast, checkout our Careers page if you would like to join us.</p>
 </div>
 
 <h2 style={ textAlignCenterStyling }>Our Team</h2>
@@ -25,10 +25,10 @@ export default function About() {
     <div className="card">
       <img src="https://images.unsplash.com/photo-1531539134685-27d854339120?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" alt="Jane" style= { styling } />
       <div className="container">
-        <h2>Jane Doe</h2>
-        <p className="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>jane@example.com</p>
+        <h2>Pavika ash</h2>
+        <p className="title">CEO</p>
+        <p>We believe in not only developing professionals, but cultivating individuals that have purpose and are energized by their task	.</p>
+        <p>pavika@example.com</p>
         <p><button className="button">Contact</button></p>
       </div>
     </div>
@@ -38,10 +38,10 @@ export default function About() {
     <div className="card">
       <img src="https://images.unsplash.com/photo-1580894908361-967195033215?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" alt="Mike" style= { styling } />
       <div className="container">
-        <h2>Mike Ross</h2>
-        <p className="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
+        <h2>Jomy Ross</h2>
+        <p className="title">Director</p>
+        <p>Join us if you wish to make revolution in the world.</p>
+        <p>jomy@example.com</p>
         <p><button className="button">Contact</button></p>
       </div>
     </div>
@@ -51,10 +51,10 @@ export default function About() {
     <div className="card">
       <img src="https://images.unsplash.com/photo-1573164713988-8665fc963095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80" alt="John" style= { styling } />
       <div className="container">
-        <h2>John Doe</h2>
+        <h2>Malina Gau</h2>
         <p className="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
+        <p>Reimage the business with us.</p>
+        <p>malina@example.com</p>
         <p><button className="button">Contact</button></p>
       </div>
     </div>
